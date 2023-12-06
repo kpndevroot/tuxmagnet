2439a53c10bcc87e1cd0d5e73f541bf7aed9106a
kpndevroot <vishnukpn884@gmail.com>
2023-12-06 22:58:21 +0530

    add ubuntu apps

--- tuxmagnet.sh
+++ tuxmagnet.sh
@@ -4,7 +4,7 @@ declare -A installed_status
 
 os_options=("Arch" "Ubuntu" "Fedora")
 Arch_apps=("firefox" "google-chrome" "visual-studio-code-bin" "vim" "vlc" "postgresql" "pulseaudio" "jdk-openjdk" "mongodb-bin" "mongodb-compass" "mongodb-tools-bin" "android-studio" "android-tools" "brave-bin" "geekbench")
-Ubuntu_apps=("build-essential" "wget" "curl" "vim" "firefox")  # Add applications for Ubuntu
+Ubuntu_apps=("build-essential" "wget" "curl" "vim" "firefox" "vlc" "default-jre default-jdk")  # Add applications for Ubuntu
 Fedora_apps=("firefox" "google-chrome-stable" "code")
 
 welcome_message() {
